cbde89095f9a8a038370d98987ab8fc59f8072bc
twodigitss <torrescervantesle@gmail.com>
2024-01-23 14:24:41 -0600

    small update: new features
    listed changes are inside config.py rn, new update release soon if everything goes well

--- config.py
+++ config.py
@@ -1,46 +1,64 @@
-#Options to print this fetch
-''' NOTES:
-    1. DO NOT make the custom_ascii string into a f-string. It will
-       still take care about the inner curly bracets and take them as 
-       characters and therefore, will impact the identation for shorter lines.
-    2. Due to 1st point, custom ascii arts are colorlesss 
-    3. MAKE SURE your custom_ascii does not have weird escape chars. 
+''' -- CONFIGURATION FILE (if it were not more obvious) --
+    NOTES:
+    1. MAKE SURE your custom ascii does not have weird escape chars. 
+    2. Suggest and report bugs or whatever in discussions/issues on github
     
     EXTRA: if you are facing problems with blank spaces in custom ascii, take this
-    empty char made for filling some ascii arts, it will surely fix it
+    empty chars made for filling some ascii arts, it will surely fix it
     emptychars="⠀⠀⠀⠀⠀⠀"
-
+    
+    added:  more information available to display
+            now you can choose the position of the color blocks
+            improved static color set behavior, 
+            old show_ascii is now a string instead of boolean (is more flexible to manage now)
 '''
+
+''' INFO CONFIGURATION '''
+#Shows in order what information is displayed
+#Items: "distro","owner","kernel","de","pkg","shell","uptime","user","host","cpu","model","gpu","ram",
+display=["distro","owner","de","pkg","kernel","shell","uptime",]
+
 #Enables/disables random phrases at the top of the fetch
 show_phrase=True
 
+#Values: "off", "default" or "custom"
+show_ascii="default"
+
+#some random phrases to launch every time its executed
+somephrases=[
+    "Hellow r/Unixporn! look at this!!",
+    "Energy(e) = milk(m) * coffe(c) ^2",
+    "Welcome back!! hope you enjoy today (:",
+    "Hey there! Ready to dive into the day?",
+    "Keep calm and be happy today c:",
+    "Inspired in bunnyfetch!! check it out!",
+    "Peace and love for everyone <3",
+    "Hey ya!, ready to brighten up the day? (:", 
+    "This fetch is owned now by Microsoft",
+    "Stay high and spread positive energy!!",
+]
+
+''' STYLES CONFIGURATION '''
 #Indent the phrase to left instead right on custom ascii
 phraseToLeft=False
 
-#Enables/disables every ascii art
-show_ascii=True
+#Enable/disable color randomization
+randomize_colors=True
+
+#If randomize_colors is false, you can set the order of the colors
+static_color_set=["cyan","blue","black","red","purple","green","white","yellow"]
+
+#Values "off" "left" "bottom" 
+color_block_position="left"
 
 #Enables/Disables icons from NerdFonts.com
 show_icons=True
 
-#shows your distro icon instead the stock one in "icons" list
+#Shows your distro icon instead the stock one in "icons" list
 override_distro_icon=True
 
 #Icons that are shown alsongside display variable
-icons=["","","󰒔","󰙀","󰏓","",""]
-
-#Shows in order what information is displayed
-display=["os","host","kernel","de","pkg","shell","uptime"]
-
-#Enable/disable normal behavior of this app
-randomize_colors=True
-
-#If randomize_colors is false, you can set the order of the colors
-#this column is:  os    host   krn   de   pkg    sh    upt   NA
-static_color_set=["cyn","ble","blk","red","prp","grn","wht","ylw"]
-
-#Values: ["custom" or "default"]
-prioritize_ascii="default"
+icons=["󰹑","","󰒔","󰙀","󰏓","","","","","󰋊","󰘚","","󰌢"]
 
 #Paste here your ascii
 custom_ascii='''
@@ -55,16 +73,3 @@ custom_ascii='''
 ⣿⣿⣿⣇⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿
 ⣿⣿⣿⣿⣷⣤⣶⣶⠀⣿⣿⣿⣿⣿
 '''
-
-#some random phrases to launch every time its executed
-somephrases=[
-    "Hellow r/Unixporn! look at this!!",
-    "Welcome back!! hope you enjoy today (:",
-    "Hey there! Ready to dive into the day?",
-    "Keep calm and be happy today c:",
-    "Inspired in bunnyfetch!! check it out!",
-    "Peace and love for everyone <3",
-    "Hey ya!, ready to brighten up the day? (:", 
-    "This fetch is owned now by Microsoft",
-    "Stay high and spread positive energy!!",
-]
