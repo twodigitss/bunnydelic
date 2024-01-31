003fe22f9875223c168bdab8e1b6f2478d9aa2bf
twodigitss <torrescervantesle@gmail.com>
2024-01-30 22:14:32 -0600

    Added more functionality and some bugfixes

--- config.py
+++ config.py
@@ -6,47 +6,32 @@
     EXTRA: if you are facing problems with blank spaces in custom ascii, take this
     empty chars made for filling some ascii arts, it will surely fix it
     emptychars="⠀⠀⠀⠀⠀⠀"
-    
+
     added:  more information available to display
             now you can choose the position of the color blocks
             improved static color set behavior, 
-            old show_ascii is now a string instead of boolean (is more flexible to manage now)
+            improved show_ascii parameters for more flexibility
+            color bug on "bottom" mode fixed 
 '''
 
-''' INFO CONFIGURATION '''
-#Shows in order what information is displayed
-#Items: "distro","owner","kernel","de","pkg","shell","uptime","user","host","cpu","model","gpu","ram",
-display=["distro","owner","de","pkg","kernel","shell","uptime",]
-
-#Enables/disables random phrases at the top of the fetch
+#Enables/disables phrases
 show_phrase=True
+
+#Indent phrases to left instead right on custom ascii
+phraseToLeft=False
+
+#Items: distro,owner,kernel,de,pkg,shell,uptime,user,host,cpu,model,gpu,ram,
+display=["distro","owner","kernel","de","pkg","shell","uptime"]
 
 #Values: "off", "default" or "custom"
 show_ascii="default"
 
-#some random phrases to launch every time its executed
-somephrases=[
-    "Hellow r/Unixporn! look at this!!",
-    "Energy(e) = milk(m) * coffe(c) ^2",
-    "Welcome back!! hope you enjoy today (:",
-    "Hey there! Ready to dive into the day?",
-    "Keep calm and be happy today c:",
-    "Inspired in bunnyfetch!! check it out!",
-    "Peace and love for everyone <3",
-    "Hey ya!, ready to brighten up the day? (:", 
-    "This fetch is owned now by Microsoft",
-    "Stay high and spread positive energy!!",
-]
-
-''' STYLES CONFIGURATION '''
-#Indent the phrase to left instead right on custom ascii
-phraseToLeft=False
-
 #Enable/disable color randomization
 randomize_colors=True
 
-#If randomize_colors is false, you can set the order of the colors
-static_color_set=["cyan","blue","black","red","purple","green","white","yellow"]
+#Set the color order if randomize_colors is false.
+#You can repeat colors, also make sure this list is at least 4 elements long
+static_color_set=["cyan","blue","yellow","red","purple","green","white"]
 
 #Values "off" "left" "bottom" 
 color_block_position="left"
@@ -57,8 +42,8 @@ show_icons=True
 #Shows your distro icon instead the stock one in "icons" list
 override_distro_icon=True
 
-#Icons that are shown alsongside display variable
-icons=["󰹑","","󰒔","󰙀","󰏓","","","","","󰋊","󰘚","","󰌢"]
+#Icons that are shown alsongside display variable 󰞅
+icons=["󰟀","󰀎","󰒔","󰙀","󰏓","","󱑂","","󰾆","󰋊","󰘚","󰳣",""]
 
 #Paste here your ascii
 custom_ascii='''
@@ -73,3 +58,17 @@ custom_ascii='''
 ⣿⣿⣿⣇⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿
 ⣿⣿⣿⣿⣷⣤⣶⣶⠀⣿⣿⣿⣿⣿
 '''
+
+#some random phrases to launch every time its executed
+somephrases=[
+    "Hellow r/Unixporn! look at this!!",
+    "Energy(e) = milk(m) * coffe(c) ^ 2",
+    "Welcome back!! hope you enjoy today (:",
+    "Hey there! Ready to dive into the day?",
+    "Keep calm and be happy today c:",
+    "Inspired in bunnyfetch!! check it out!",
+    "Peace and love for everyone <3",
+    "Hey ya!, ready to brighten up the day? (:", 
+    "This fetch is owned now by Microsoft",
+    "Stay high and spread positive energy!!",
+]
