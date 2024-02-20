027285c72cc96f234f80027b3bd4ddee9b4ec100
twodigitss <torrescervantesle@gmail.com>
2024-02-20 00:18:04 -0600

    Added text formatting

--- config.py
+++ config.py
@@ -15,21 +15,28 @@ show_phrase=True
 #Indent phrases to left instead right on custom ascii
 phraseToLeft=False
 
-#Items: distro,owner,kernel,de,pkg,shell,uptime,user,host,cpu,model,gpu,ram,
-display=["distro","owner","kernel","de","pkg","shell","uptime"]
+#values: normal, italic, bold, underline 
+phraseStyle = "normal"
 
-#Values: "off", "default" or "custom"
+#Items: cpu,de,distro,gpu,host,kernel,model,owner,pkg,ram,shell,uptime,user
+display=["distro","user","kernel","shell","pkg","ram","uptime"]
+
+#values: normal, italic, bold, underline
+displayStyle = "normal"
+informationStyle = "normal"
+
+#Values: off, default, custom
 show_ascii="default"
+
+#Values "off" "left" "bottom" 
+color_block_position="left"
 
 #Enable/disable color randomization
 randomize_colors=True
 
 #Set the color order if randomize_colors is false.
 #You can repeat colors, also make sure this list is at least 4 elements long
-static_color_set=["cyan","blue","yellow","red","purple","green","white"]
-
-#Values "off" "left" "bottom" 
-color_block_position="left"
+static_color_set=["purple","red","black","blue","green","white","cyan","yellow"]
 
 #Enables/Disables icons from NerdFonts.com
 show_icons=True
@@ -38,7 +45,7 @@ show_icons=True
 override_distro_icon=True
 
 #Icons that are shown alsongside display variable 󰞅
-icons=["󰟀","󰀎","󰒔","󰙀","󰏓","","󱑂","","󰾆","󰋊","󰘚","󰳣",""]
+icons=["󰟀","󰀎","󰒔","󰙀","󰏓","","󱑂","","󰾆","","󰘚","󰳣",""]
 
 #Paste here your ascii
 custom_ascii='''
@@ -61,9 +68,11 @@ somephrases=[
     "Welcome back!! hope you enjoy today (:",
     "Hey there! Ready to dive into the day?",
     "Keep calm and be happy today c:",
+    "Fridays I go painting in the Louvre!!",
     "Inspired in bunnyfetch!! check it out!",
     "Peace and love for everyone <3",
     "Hey ya!, ready to brighten up the day? (:", 
     "This fetch is owned now by Microsoft",
     "Stay high and spread positive energy!!",
+    "Rest in Peace ArchLabs, we will miss you",
 ]
